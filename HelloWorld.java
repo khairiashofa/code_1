cffc93f261cc8c720b1ac20f2d00560d01ccb237
khairiashofa <khairiashofa.apk@gmail.com>
2024-09-21 21:29:26 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * HelloWorld
+ * Nama: Khairia Shofa
+ * NIM : 2411016220028
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        //string menggunakan tanda petik 2 (" ")
+        System.out.println("Hello World");
+        System.out.println("Nama: Khairia Shofa");
+        System.out.println("NIM: 2411016220028");
+
+        //angka (integer/double/float) tidak perlu tanda petik
+        System.out.println(13);
+        System.out.print(true);
+
+        //char menggunakan tanda petik 1 (' ')
+        System.out.print('A');
+}
+}
